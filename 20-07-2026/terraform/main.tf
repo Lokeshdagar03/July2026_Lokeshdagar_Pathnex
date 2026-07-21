97b9e65b2ef1da96dcf5e6ad6c86099abda3d169
EC2 Default User <ec2-user@ip-172-31-1-120.ap-south-1.compute.internal>
2026-07-21 10:30:56 +0000

    Day 20 - Security, Hardening, and Compliance

---
+++ 20-07-2026/terraform/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+
+ region = "ap-south-1"
+
+}
+
+
+resource "aws_security_group" "pathnex_sg" {
+
+
+name_prefix = "pathnex-security-"
+
+
+description = "Allow secure web access"
+
+
+
+ingress {
+
+
+from_port = 22
+
+to_port = 22
+
+protocol = "tcp"
+
+cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+
+
+ingress {
+
+
+from_port = 80
+
+to_port = 80
+
+protocol = "tcp"
+
+cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+
+
+egress {
+
+
+from_port = 0
+
+to_port = 0
+
+protocol = "-1"
+
+cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+}
